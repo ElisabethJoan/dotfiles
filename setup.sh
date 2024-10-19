c6c18574f595e7582e1d3c0ac0857edda8805d59
Elisabeth Joan <Elisabeth.L.Joan@gmail.com>
2024-10-19 14:09:41 +1000

    add scripts to update repo with config changes and setup dev env with repo configs

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm ~/.zshenv
+cp .zshenv ~/.zshenv
+rm ~/.zshrc
+cp .zshrc ~/.zshrc
+
+mkdir -p ~/.config/alacritty
+cp .config/alacritty/alacritty.toml ~/.config/alacritty
+
+mkdir -p ~/.config/nvim/lua/configs
+mkdir -p ~/.config/nvim/lua/plugins
+cp .config/nvim/lua/configs/lspconfig.lua ~/.config/nvim/lua/configs
+cp .config/nvim/lua/plugins/init.lua ~/.config/nvim/lua/plugins
+cp .config/nvim/lua/chadrc.lua ~/.config/nvim/lua
+cp .config/nvim/lua/options.lua ~/.config/nvim/lua
